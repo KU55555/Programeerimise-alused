f70033bf133f829f767ab149448c8703b0abb368
KU55555 <kasparuibo@gmail.com>
2022-11-16 10:51:10 +0200

    Football players number

--- 15.11.2022/ConsoleApp1/ConsoleApp1/Program.cs
+++ 15.11.2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -6,8 +7,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your favorite football player t-shirt number!");
+           string number = Console.ReadLine();
+            int numberParsed = Int16.Parse(number);     
+          //int 16 et parsida
+
+            // teen siia if ja else loogikaga vastused 
+            if (numberParsed < 50)
+            {
+                Console.WriteLine("Cool player");
+                
+                }
+            
+            
+            
+            
+            else if (numberParsed > 50)
+            {
+                Console.WriteLine("Not cool player");
         }
+
+
+
+
+
+
+
+
+
+    }
     }
 }
