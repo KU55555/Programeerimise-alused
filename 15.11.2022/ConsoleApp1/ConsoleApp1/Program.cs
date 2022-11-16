541d5685e4beba6fceaf12edfead4084022929f8
KU55555 <kasparuibo@gmail.com>
2022-11-16 11:14:35 +0200

    eeeeeee

--- 15.11.2022/ConsoleApp1/ConsoleApp1/Program.cs
+++ 15.11.2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,32 +9,85 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Enter your favorite football player t-shirt number!");
            string number = Console.ReadLine();
-            int numberParsed = Int16.Parse(number);     
-          //int 16 et parsida
+            int numberParsed = Int16.Parse(number);
+            //int 16 et parsida
 
             // teen siia if ja else loogikaga vastused 
+
+            if (numberParsed <= 50)
+            {
+                Console.WriteLine("See nr on võrdne või väiksem 50-st");
+            }
+            else
+            {
+                Console.WriteLine("Kõik ülejäänud numbrid.");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             if (numberParsed < 50)
             {
                 Console.WriteLine("Cool player");
-                
-                }
-            
-            
-            
-            
-            else if (numberParsed > 50)
+
+          }
+            else if (numberParsed > 50 && numberParsed < 100)
             {
                 Console.WriteLine("Not cool player");
+            }
+
+
+            
+           
+            
+            else if (numberParsed  > 1 && numberParsed < 50)
+            {
+                Console.WriteLine("Cool player");
+            }
+     
+       
+        
+        
+        
+        
         }
 
-
-
-
-
-
-
-
-
     }
-    }
+
 }
+
+
+
